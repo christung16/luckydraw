b0f7314f8680f1f02784913a5fe3ac39a7329e28
christung16 <tungchris@gmail.com>
2024-11-02 01:58:37 +0800

    luckydraw

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+/* function draw() {
+  luckyNumber = document.getElementById("lucky-number")
+  randomnumber = Math.random()
+  luckyNumber.innerHTML = Math.floor(randomnumber*100+1)
+  console.log("lucky-number: ",randomnumber)
+}
+ */
+
+
+/* function draw() {
+	luckyNumber = document.getElementById("lucky-number");
+  luckyNumber.innerHTML = Math.floor(Math.random() * 100 + 1);
+
+} */
